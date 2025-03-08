5d9ab690c6419805cf86f9a513afd9a74bf6b50a
Eliot Colas <eliot.colas29120@gmail.com>
2025-03-08 16:56:12 +0100

    git structure

--- main_Lab1.m
+++ main_Lab1.m
@@ -6,21 +6,6 @@ addpath(genpath("functions"))
 addpath figures/
 addpath datas/
 
-%% Observe only certain mode
-lx = 1; ly = 1;
-% z = linspace(0,1,10);
-z=0.1;
-f = linspace(1,1000,1000-1) ;
-
-N_modes = [40,40];
-xM = 1; yM = 1; % receiver
-rS = [0.,0.,0]; %source
-
-%G = Gf_duct([xM,yM],rS, z, [lx,ly], f, N_modes,[1;0],1); % use either N_modes array and empty 
-G = Gf_duct([xM,yM],rS, z, [lx,ly], f, N_modes,[],2); % use either N_modes array and empty 
-
-
-plot(f,20*log10(abs(G)/2e-5))
 
 
 % -------------------------------------------
@@ -48,7 +33,7 @@ compute_convergence
 plot_convergence
 
 
-%% plot velocity
+%% plot velocities
 f = 1:10:1000;
 [Cp,Cg,nx] = calc_modes_vel([0.7,1],f,[9,9]);
 
