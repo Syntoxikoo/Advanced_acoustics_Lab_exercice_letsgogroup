c2228c45604fc0d8aab9acb3ae4e984330de0dea
Eliot Colas <eliot.colas29120@gmail.com>
2025-03-25 09:07:02 +0100

    change of the part 4

--- scripts/Lab_3/sound_field_in_room.m
+++ scripts/Lab_3/sound_field_in_room.m
@@ -3,7 +3,7 @@ TR60 = 3.5;
 c0 = 343;
 fs=  schroeder_rect_room(room, TR60,'c0', c0);
 % fs = find_f_modes(1,1,1);
-fs = 700;
+% fs = 700;
 
 lambs = c0 / fs;
 N = 1000;
@@ -14,11 +14,11 @@ z =  lambs/2 + (room(3)- lambs) .*rand(N,1);
 
 % x = room(1).*rand(N,1);
 % y = room(2).*rand(N,1);
-% z = zeros(N,1);
+% z = room(3).*rand(N,1);
 rS = [0,0,0];
 rM = [x,y,z];
 
-G = green_func_room_lab3(rM,rS,room, 'f', fs,'max_mode',10, 'absorption', false, 'T60', TR60,'no_const',true);
+G = green_func_room_lab3(rM,rS,room, 'f', fs,'max_mode',10, 'absorption', true, 'T60', TR60,'no_const',true);
 
 figure;
 scatter(x,y, 10, 20*log10(abs(G)/2e-5))
