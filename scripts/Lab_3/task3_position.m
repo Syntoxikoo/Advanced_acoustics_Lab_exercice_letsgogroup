29d2e101ce7785391bc2f4d3a452e06a2de63e0b
AurelienLeHawkinho <aurelien06.cresp@gmail.com>
2025-03-28 18:10:06 +0100

    added theoretical curves histogram + modificatoins

--- scripts/Lab_3/task3_position.m
+++ scripts/Lab_3/task3_position.m
@@ -11,11 +11,21 @@ k = omega / c;
 z_values = linspace(0, Lz, Npoints); % space points
 p2_values = zeros(size(z_values));
 
-% calculating p_2 for each position as a sum of every random wave
+phi_random = [];
+theta_random = [];
+
+rand_idx = randi([1, Npoints]);
+
 for idx = 1:length(z_values)
     r = z_values(idx);
-    phi_i = rand(1, Nwaves) * 2 * pi; % random phases
-    theta_i = acos(2 * rand(1, Nwaves) - 1); % random angles
+    phi_i = rand(1, Nwaves) * 2 * pi; 
+    theta_i = acos(2 * rand(1, Nwaves) - 1); 
+    
+    if idx == rand_idx
+        phi_random = phi_i;
+        theta_random = theta_i;
+    end
+    
     sum_exp = sum(exp(-1j * (k * r * cos(theta_i) + phi_i))); 
     p2_values(idx) = (1/Nwaves) * abs(sum_exp)^2;
 end
@@ -25,7 +35,6 @@ fprintf('Size of p2_values array: %d x %d\n', size(p2_values,1), size(p2_values,
 p_ref = 2 * 10^(-5);
 pref_sq = p_ref^2;
 
-% Relative sound pressure level (SPL)
 figure;
 plot(z_values, 10*log10(p2_values / pref_sq), 'LineWidth', 1.5);
 grid on;
@@ -35,18 +44,15 @@ ylabel('L_p (dB SPL)');
 
 %% stats
 
-%p_rms
 p_rms = sqrt(p2_values/2);
 
 mean_p = mean(p_rms);
 std_p = std(p_rms);
-rel_std_p = std_p / mean_p * 100; % relative std (%)
+rel_std_p = std_p / mean_p * 100; 
 
 Lp_values = 20 * log10(p_rms / p_ref);
 mean_Lp = mean(Lp_values);
 std_Lp = std(Lp_values);
-
-% Histograms (p2 and dB) for one sound field
 
 N_bins = 30;
 figure;
@@ -66,6 +72,48 @@ title(['Histogram of SPL across spatial points ' ...
     'Std = ' num2str(std_Lp, '%.2f') ' dB)']);
 xlabel('L_p (dB SPL)');
 ylabel('Counts');
+
+
+% stats on phi and theta
+theta_bins = linspace(0, pi, N_bins);
+theta_hist = hist(theta_random, theta_bins);
+theta_pdf_theoretical = sin(theta_bins) / 2;
+
+theta_hist_normalized = theta_hist / sum(theta_hist); 
+max_theta_hist = max(theta_hist_normalized); 
+theta_pdf_theoretical_scaled = (theta_pdf_theoretical / max(theta_pdf_theoretical)) * max_theta_hist;
+
+% Phi distribution
+phi_bins = linspace(0, 2*pi, N_bins);
+phi_hist = hist(phi_random, phi_bins);
+phi_pdf_theoretical = ones(size(phi_bins)) / (2*pi); 
+
+% Normalization
+phi_hist_normalized = phi_hist / sum(phi_hist); 
+max_phi_hist = max(phi_hist_normalized); 
+phi_pdf_theoretical_scaled = (phi_pdf_theoretical / max(phi_pdf_theoretical)) * max_phi_hist;
+
+% Plot theta histogram with theoretical curve
+figure;
+bar(theta_bins, theta_hist_normalized, 'hist'); 
+hold on;
+plot(theta_bins, theta_pdf_theoretical_scaled, 'r', 'LineWidth', 2);
+grid on;
+title('Histogram of theta at Random z point with theoretical curve');
+xlabel('Theta Angle (radians)');
+ylabel('Probability density function');
+legend('Observed Distribution', 'Scaled Theoretical Distribution');
+
+% Plot phi histogram with theoretical curve
+figure;
+bar(phi_bins, phi_hist_normalized, 'hist'); 
+hold on;
+plot(phi_bins, phi_pdf_theoretical_scaled, 'r', 'LineWidth', 2);
+grid on;
+title('Histogram of Phi at random z Point with theoretical curve');
+xlabel('Phase Angle (radians)');
+ylabel('Probability density function');
+legend('Observed Distribution', 'Scaled Theoretical Distribution');
 
 %% OLD VERSION OF STATS PART
 
