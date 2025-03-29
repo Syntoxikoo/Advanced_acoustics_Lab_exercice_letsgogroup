8841c9c67130330fc5fd436675dd153b1de4b4ac
Silas Stuhldreier <silasstuhldreier@MacBook-Pro-von-Silas.local>
2025-03-29 12:13:54 +0100

    good morning

--- scripts/Lab_3/task2.m
+++ scripts/Lab_3/task2.m
@@ -2,45 +2,58 @@
 clear;
 clc
 
-krMAX= 8*pi;
+krMAX= 8*pi; % maximum of kr for simulations
 
 N = [1 10 100]; % Number of Waves
 M = [10 100 1000]; % Number of Monte carlo simulations for averaging
 
 kr = linspace(0,krMAX,1000); % kr Vector
 
-yM=zeros(length(M),length(kr));
+% initializing matrices
+yM=zeros(length(M),length(kr)); 
 yN=zeros(length(N),length(kr));
 
-for m=1:length(M)
-    p1p2=zeros(M(m),length(kr));
+% Calculate for fixed Number of waves and different number of MC-Simulations
+for m=1:length(M) 
+
+    % initializing matrices
+    p1p2=zeros(M(m),length(kr)); 
     phi=zeros(M(m),N(length(N)));
     msP=zeros(1,M(m));
 
+    % calculate array of MC-Simulations
     for i=1:M(m)
         [p1p2(i,:),phi(i,:)] = spatCorr(kr,N(length(N)));
         msP(i) = meanSqP(N(length(N)),phi(i,:));
     end
-        
+    
+    % average over number of MC-Simulations
     avgmsP=mean(msP);
     avgp1p2=mean(p1p2,1);
     
+    % Calculate normalized spatial correlation
     yM(m,:) = avgp1p2 ./ avgmsP;
 end
 
-for n=1:length(N)
+% Calculate for fixed Number of MC-Simulations  and different number of waves
+for n=1:length(N) 
+
+    % initializing matrices
     p1p2=zeros(M(length(M)),length(kr));
     phi=zeros(M(length(M)),N(n));
     msP=zeros(1,M(length(M)));
 
+    % calculate array of MC-Simulations
     for i=1:M(length(M))
         [p1p2(i,:),phi(i,:)] = spatCorr(kr,N(n));
         msP(i) = meanSqP(N(n),phi(i,:));
     end
-        
+    
+    % average over number of MC-Simulations
     avgmsP=mean(msP);
     avgp1p2=mean(p1p2,1);
-    
+   
+    % Calculate normalized spatial correlation
     yN(n,:) = avgp1p2 ./ avgmsP;
 end
 
@@ -53,6 +66,7 @@ heightScale = 0.75; % Adjust height scaling if needed
 
 % ------------------------------------------------------------------------------------------------------------
 [columnwidth, ~] = get_widths();
+columnwidth=800;
 height = get_height() * heightScale; 
 fig = figure("Position", [0, 0, columnwidth, height], "Units", "points");
 tiled = tiledlayout(nrows, ncols, "TileSpacing", "tight", "Padding", "loose");
