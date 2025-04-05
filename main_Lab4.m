27e13647ddca919de47b93083b49f4af3e5d3afc
Silas Stuhldreier <silasstuhldreier@MacBook-Pro-von-Silas.local>
2025-04-05 12:45:01 +0200

    new plotting

--- main_Lab4.m
+++ main_Lab4.m
@@ -8,4 +8,6 @@ addpath(genpath("datas"))
 %% ---- Import Data
 
 calcSP;
-plotSP;
+plotSP(fc,P_dipole,P_dipole_wall,'Dipole');
+plotSP(fc,P_highImp,P_highImp_wall,'High Impedance Source');
+plotSP(fc,P_lowImp,P_lowImp_wall,'Low Impedance Source');
--- scripts/Lab_4/plotSP.m
+++ scripts/Lab_4/plotSP.m
@@ -1,9 +1,17 @@
-%% plotting sound power
+function []=plotSP(fc,P1,P2,name)
+% function to plot the sound power of two measurements. one regular and one
+% at a wall
+% fc = 1/3 octave band center frequencies
+% P1 = first Power spectrum
+% P2 = second power spectrum
+% name = title of graph
+
+% plotting sound power
 
 % Define the number of rows and columns for tiled layout
 nrows = 1;
-ncols = 3;
-heightScale = 0.5; % Adjust height scaling if needed
+ncols = 1;
+heightScale = 0.65; % Adjust height scaling if needed
 
 % ------------------------------------------------------------------------------------------------------------
 [columnwidth, ~] = get_widths();
@@ -19,14 +27,14 @@ nexttile
 x = 1:length(fc); % Generates equally spaced x-values
 
 % Set X Y...
-Leg(1) = plot(x, P_highImp,"LineStyle",'-',"LineWidth", 1.0, "Color", corder(1,:),"DisplayName","average"); 
+Leg(1) = plot(x, P1,"LineStyle",'-',"LineWidth", 1.0, "Color", corder(1,:),"DisplayName","average"); 
 
 grid on; hold on;
 
-Leg(2) = plot(x, P_highImp_wall,"LineStyle",'-',"LineWidth", 1.0, "Color", corder(2,:),"DisplayName","at wall"); 
+Leg(2) = plot(x, P2,"LineStyle",'-',"LineWidth", 1.0, "Color", corder(2,:),"DisplayName","at wall"); 
 
 % Set axis limits 
-% ylim([0, 1]) 
+% ylim([0, 60]) 
 % xlim([10, 1000]) 
 
 % Adjust x-axis labels to show original frequency values
@@ -34,63 +42,11 @@ xticks(x);           % Set ticks to match x indices
 xticklabels(fc);   % Label them with the actual frequency values
 
 % Add title 
-title('High impedance source');
+title(name);
 
 % Uncomment these if individual tile labels are preferred
-% xlabel('X-axis Label');
-% ylabel('Y-axis Label');
-
-% ------------------------------------- Second Tile ---------------------------------------------------------
-nexttile 
-
-% Set X Y...
-Leg(1) = plot(x, P_lowImp,"LineStyle",'-',"LineWidth", 1.0, "Color", corder(1,:),"DisplayName","average"); 
-
-grid on; hold on;
-
-Leg(2) = plot(x, P_lowImp_wall,"LineStyle",'-',"LineWidth", 1.0, "Color", corder(2,:),"DisplayName","at wall"); 
-
-% Set axis limits 
-% ylim([0, 1]) 
-% xlim([10, 1000]) 
-
-% Adjust x-axis labels to show original frequency values
-xticks(x);           % Set ticks to match x indices
-xticklabels(fc);   % Label them with the actual frequency values
-
-% Add title (optional)
-title('Low impedance source');
-
-% Uncomment these if individual tile labels are preferred
-% xlabel('X-axis Label');
-% ylabel('Y-axis Label');
-
-% ------------------------------------- third Tile ---------------------------------------------------------
-nexttile 
-
-% Set X Y...
-Leg(1) = plot(x, P_dipole,"LineStyle",'-',"LineWidth", 1.0, "Color", corder(1,:),"DisplayName","average"); 
-
-grid on; hold on;
-
-Leg(2) = plot(x, P_dipole_wall,"LineStyle",'-',"LineWidth", 1.0, "Color", corder(2,:),"DisplayName","at wall"); 
-
-% Set axis limits 
-% ylim([0, 1]) 
-% xlim([10, 1000]) 
-
-% Adjust x-axis labels to show original frequency values
-xticks(x);           % Set ticks to match x indices
-xticklabels(fc);   % Label them with the actual frequency values
-
-% Add title (optional)
-title('Dipole');
-
-legend("location","southeast");
-
-% Uncomment these if individual tile labels are preferred
-% xlabel('X-axis Label');
-% ylabel('Y-axis Label');
+xlabel('Frequency in Hz');
+ylabel('Sound Power in dB re 1pW');
 
 % ------------------------------------- Misc for Figure -----------------------------------------------------------
 
@@ -98,10 +54,11 @@ legend("location","southeast");
 % leg.Layout.Tile = 'north'; 
 
 % Add common X and Y axis labels for all tiles
-xlabel(tiled, 'Common X-axis Label');
-ylabel(tiled, 'Common Y-axis Label');
+% xlabel(tiled, 'Common X-axis Label');
+% ylabel(tiled, 'Common Y-axis Label');
 
 % Save the figure in EPS format (modify file name)
-saveas(gcf, 'figures/namefig.eps', "epsc");
+saveas(gcf, sprintf('figures/%s_SP.eps',name), "epsc");
+end
 
 
